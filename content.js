d9f579c5c9d8b74625172a0fc76766b57335c323
DementiaGaming <sukhrathansidhu@outlook.com>
2025-03-26 21:24:22 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
-let random = Math.floor(Math.random() * 1) + 1;
+let random = Math.floor(Math.random() * 10) + 1;
 let time = Math.floor(Math.random() * 35400) + 1;
-let audio = new Audio("audio/segway.mp3");
+
 if (random == 10) {
-    audio.play();
     window.location.replace(`https://youtu.be/qy4ehIfEj8I?si=MJ96nzok1dJKZMkv?t=${time}s`);
 }
